688b1170e6775522720a6eee04fe3458e339db72
hextraza <sebastiancbenjamin@gmail.com>
2021-09-01 16:00:45 -0700

    New workflow

--- nimble/__main__.py
+++ nimble/__main__.py
@@ -6,14 +6,14 @@ import subprocess
 import requests
 import stat
 import json
+import pathlib
 import zipfile
 import pysam
 
 from sys import platform
 
 from nimble.types import Config
-from nimble.parse import parse_fasta, parse_filter_config
-from nimble.remote import get_remote_lib
+from nimble.parse import parse_fasta, parse_filter_config, parse_csv
 from nimble.usage import print_usage_and_exit
 from nimble.utils import get_exec_name_from_platform
 from nimble.reporting import report
@@ -21,49 +21,26 @@ from nimble.reporting import report
 align_tries = 10
 align_tries_threshold = 0
 
-# Take human-editable config json files and compile into a single minified file for the aligner
-def compile_config(reference_output_path, config_output_path, compiled_json_path):
-    with open(reference_output_path, "r") as ref, open(
-        config_output_path, "r"
-    ) as conf, open(compiled_json_path, "w") as comp:
-        reference = json.load(ref)
-        config = json.load(conf)
-        json.dump([config, reference], comp)
-
 
 # Generate and write human-editable config json files to disk
-def create_editable_config(seq_path, reference_output_path, config_output_path):
+def generate(file, output_path):
+    #TODO process FASTA + CSV
     data = None
     config = None
 
-    (data, config) = parse_fasta(seq_path)
+    if pathlib.Path(file).suffix == ".fasta":
+        (data, config) = parse_fasta(file)
+    elif pathlib.Path(file).suffix == ".csv":
+        (data, config) = parse_csv(file)
 
     # Write reference and default config to disk
-    with open(reference_output_path, "w") as f:
-        json.dump(data.__dict__, f, indent=2)
-
-    with open(config_output_path, "w") as f:
-        json.dump(config.__dict__, f, indent=2)
-
-
-# Generate and write human-editable config json files to disk
-def create_editable_config_remote(remote_seq_path, reference_output_path, config_output_path):
-    data = None
-    config = None
-
-    (data, config) = get_remote_lib(remote_seq_path)
-
-    # Write reference and default config to disk
-    with open(reference_output_path, "w") as f:
-        json.dump(data.__dict__, f, indent=2)
-
-    with open(config_output_path, "w") as f:
-        json.dump(config.__dict__, f, indent=2)
+    with open(output_path, "w") as f:
+        json.dump([ config.__dict__, data.__dict__], f, indent=2)
 
 
 # Given the name of a release, download the platform-specific executable from that release.
 # Given no name, default to the most recent release.
-def download_aligner(release):
+def download(release):
     exec_name = get_exec_name_from_platform()
 
     url = ""
@@ -139,13 +116,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:  # Ensure we can index sys.argv[1]
         print_usage_and_exit()
     elif sys.argv[1] == "download" and len(sys.argv) <= 3:
-        download_aligner(sys.argv[2:])
-    elif sys.argv[1] == "generate" and len(sys.argv) == 5:
-        create_editable_config(sys.argv[2], sys.argv[3], sys.argv[4])
-    elif sys.argv[1] == "remote-generate" and len(sys.argv) == 5:
-        create_editable_config_remote(sys.argv[2], sys.argv[3], sys.argv[4])
-    elif sys.argv[1] == "compile" and len(sys.argv) == 5:
-        compile_config(sys.argv[2], sys.argv[3], sys.argv[4])
+        download(sys.argv[2:])
+    elif sys.argv[1] == "generate" and len(sys.argv) == 4:
+        generate(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == "align":
         align(sys.argv[2:])
     elif sys.argv[1] == "report" and len(sys.argv) == 5:
--- nimble/parse.py
+++ nimble/parse.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 import pandas as pd
 
@@ -7,6 +8,7 @@ from io import StringIO
 
 from nimble.types import Data, Config, DataType
 from nimble.utils import get_library_name_from_filename
+from nimble.remote import fetch_sequence, get_ids
 
 
 # Given a path to a .fasta, reads it and returns a (Data, Config) tuple with filled objects
@@ -67,3 +69,64 @@ def parse_filter_config(reference_path):
         values.append(method["value"])
 
     return (methods, values)
+
+
+def parse_csv(csv_path, has_sequences=True):
+    data = Data()
+    config = Config()
+    config.data_type = DataType.FASTA
+
+    reference_genome = get_library_name_from_filename(csv_path)
+    reference_genomes = []
+    sequence_names = []
+    nt_lengths = []
+    sequences = []
+    metadata = []
+
+    with open(csv_path) as f:
+        reader = csv.reader(f, delimiter=",", quotechar='"')
+
+        headers = next(reader)
+
+        sequence_idx = None
+        if has_sequences:
+            sequence_idx = headers.index("sequence")
+        names_idx = headers.index("name")
+
+        headers.pop(names_idx)
+
+        if has_sequences and names_idx < sequence_idx:
+            sequence_idx -= 1
+
+        headers.pop(sequence_idx)
+
+        for row in reader:
+            sequence_names.append(row.pop(names_idx))
+            reference_genomes.append(reference_genome)
+
+            if has_sequences:
+                raw_seq = row.pop(sequence_idx)
+                if "genbank://" in raw_seq:
+                    raw_seq = raw_seq.split(":")
+
+                    subset = None
+                    if len(raw_seq) == 3:
+                        subset = raw_seq[2]
+
+                    ids = get_ids(raw_seq[1].replace("//", ""))
+                    (nt_length, sequence) = fetch_sequence(ids, raw_seq, subset)
+                    nt_lengths.append(str(nt_length))
+                    sequences.append(sequence)
+                else:
+                    sequences.append(raw_seq)
+
+            if len(metadata) == 0:
+                metadata = [[] for _ in range(0, len(headers))]
+
+            for (i, col) in enumerate(row):
+                metadata[i].append(col)
+
+    data.headers.extend(headers)
+    data.columns = [reference_genomes, sequence_names, nt_lengths, sequences]
+    data.columns.extend(metadata)
+    return (data, config)
