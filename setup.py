3b4f07799a714503b22957b529147df5628ea7bb
hextraza <sebastiancbenjamin@gmail.com>
2024-06-06 11:54:02 -0700

    Update python version

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=["requests", "biopython", "pandas", "distro", "pysam", "seaborn", "jinja2", "matplotlib"],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
